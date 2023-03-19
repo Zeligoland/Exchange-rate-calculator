6e69d1775e9b6180438a164c00888c5cbaa82f30
Andrea Zapata <95andreazapata@gmail.com>
2023-03-19 14:25:23 -0500

    Bringing DOM elements to script.js

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function calculate() {
-    fetch('items.json')
-    .then(res => res.json())
-    .then(data => console.log(data));
-}
+const currencyEl_one = document.getElementById("currency-one");
+const amountEl_one = document.getElementById("amount-one");
+const currencyEl_two = document.getElementById("currency-two");
+const amountEl_two = document.getElementById("amount-two");
+
+const rateEL = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+function calculate() {}
 
 calculate();
